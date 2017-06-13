3588ffeabd5577bde57c5f0f90cea9b86f68045e
Bill Hahn <billhahng@gmail.com>
2017-06-13 14:09:24 -0500

    dataset-create.js working properly (BH)

---
+++ bq-sp-query.js
@@ -0,0 +1,36 @@
+const sqlQuery =
+  `SELECT
+  corpus, COUNT(*) as unique_words
+FROM publicdata.samples.shakespeare
+GROUP BY
+  corpus
+ORDER BY
+  unique_words DESC LIMIT 10;`;
+
+const projectId = "bill-hahn-sandbox";
+
+function queryShakespeare(projectId) {
+  // Imports the Google Cloud client library
+  const BigQuery = require('@google-cloud/bigquery');
+  // Instantiates a client
+  const bigquery = BigQuery({
+    projectId: projectId
+  });
+
+  // Query options list: https://cloud.google.com/bigquery/docs/reference/v2/jobs/query
+  const options = {
+    query: sqlQuery,
+    useLegacySql: false // Use standard SQL syntax for queries.
+  };
+
+  // Runs the query
+  bigquery
+    .query(options)
+    .then((results) => {
+      const rows = results[0];
+      printResult(rows);
+    })
+    .catch((err) => {
+      console.error('ERROR:', err);
+    });
+}
